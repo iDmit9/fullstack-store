befd676222182660660afa484a9378ca08fdc35d
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2021-05-05 22:54:26 +0600

    models-and-relationships-created

--- server/index.js
+++ server/index.js
@@ -1,45 +1,23 @@
 require('dotenv').config()
 const express = require('express')
-// const config = require('config')
-// const path = require('path')
-// const mongoose = require('mongoose')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
-// app.use(express.json({extended: true}))
+const start = async () => {
+   try {      
+      await sequelize.authenticate()
+      await sequelize.sync()
+      app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`)) 
+   } catch (error) {
+      console.log(error)
+   }
+}
 
-// app.use('/api/auth', require('./routes/auth.routes'))
-// app.use('/api/link', require('./routes/link.routes'))
-// app.use('/t', require('./routes/redirect.routes'))
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
-
-// const PORT = config.get('port') || 5000
-
-// async function start() {
-//     try {
-//         await mongoose.connect(config.get('mongoUri'), {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             useCreateIndex: true
-//         })        
-//         app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
-//     } catch (e) {
-//         console.log('Server Error', e.message)
-//         process.exit(1)
-//     }
-// }
-
-
-// start()
-
-
-app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
+start()

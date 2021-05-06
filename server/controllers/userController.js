21ac12a777d3967e0dc2e9d7186ce48101b6aac3
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2021-05-06 23:42:17 +0600

    registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,56 @@
+const bcript = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const ApiError = require('../error/ApiError')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+   return jwt.sign(
+      {id, email, role},
+      process.env.SECRET_KEY,
+      {expiresIn: '24h'}
+   )
+}
 
 class UserController {
-   async registration(req, res) {
-
+   async registration(req, res, next) {
+      const {email, password, role} = req.body
+      if (!email || !password) {
+         return next(ApiError.badRequest('Invalid email or password'))
+      }
+      const candidate = await User.findOne({
+         where: {email}
+      })
+      if (candidate) {
+         return next(ApiError.badRequest('User with this email already exist'))
+      }
+      const hashPassword = await bcript.hash(password, 5)
+      const user = await User.create({
+         email, 
+         role, 
+         password: hashPassword
+      })
+      const basket = await Basket.create({userId: user.id})
+      const token = generateJwt(user.id, user.email, user.role)
+      return res.json({token})
    }
 
-   async login(req, res) {
-
+   async login(req, res, next) {
+      const {email, password} = req.body
+      const user = await User.findOne({where: {email}})
+      if (!user) {
+         return next(ApiError.internal('User with this email was not found'))
+      }
+      let comparePassword = bcript.compareSync(password, user.password)
+      if (!comparePassword) {
+         return next(ApiError.internal('Wrong password'))
+      }
+      const token = generateJwt(user.id, user.email, user.role)
+      return res.json({token})
    }
 
    async check(req, res, next) {
-      const {id} = req.query
-      if (!id) {
-         return next(ApiError.badRequest('ID not set'))
-      }
-      res.json(id)
+      
    }
 }
 
